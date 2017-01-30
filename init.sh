5140e455b3da201a8c1c4bc6fdbb3aa452ec230d
Tonu Jaansoo <tonu.jaansoo@gmail.com>
2017-01-30 15:23:18 +0200

    err fix

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+cp local.conf build_fb/conf/local.conf
 MACHINE=var-som-mx6 DISTRO=fsl-imx-fb source var-setup-release.sh -b build_fb
 
-cp local.conf build_fb/conf/local.conf
 echo "bitbake fsl-image-qt5"
