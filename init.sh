522fdf098661a286691c59bf7e80e9b58b6bd582
Tonu Jaansoo <tonu.jaansoo@gmail.com>
2017-02-08 11:55:08 +0200

    init comment

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ cp local.conf build_fb/conf/local.conf
 MACHINE=var-som-mx6 DISTRO=fsl-imx-fb source var-setup-release.sh -b build_fb
 
 echo "bitbake fsl-image-qt5"
+echo "sudo dd if=tmp/deploy/images/var-som-mx6/fsl-image-qt5-var-som-mx6.sdcard of=/dev/sdX bs=1M"
